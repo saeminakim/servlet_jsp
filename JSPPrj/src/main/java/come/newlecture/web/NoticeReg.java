189cb9e8859ae53254bf554900bfd0ad5dd52f8a
Lily <helloimlily@gmail.com>
2021-10-10 12:53:04 +0900

    1010 사용자 입력 처리

--- JSPPrj/src/main/java/come/newlecture/web/NoticeReg.java
+++ JSPPrj/src/main/java/come/newlecture/web/NoticeReg.java
@@ -9,25 +9,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/hi")  // annotation을 이용한 URL 매핑
-public class Nana extends HttpServlet {
+@WebServlet("/notice-reg")  
+public class NoticeReg extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-		resp.setCharacterEncoding("UTF-8"); // 인코팅 방법 지정. 요청 보낼 때
-		resp.setContentType("text/html; charset=UTF-8"); // 받을 때 이렇게 받으라고 지정
+		resp.setCharacterEncoding("UTF-8"); 
+		resp.setContentType("text/html; charset=UTF-8"); 
+		req.setCharacterEncoding("UTF-8");  //입력값을 UTF-8로 인식
 
 		PrintWriter out = resp.getWriter();
 
-		String tmp = req.getParameter("cnt");
+		String title = req.getParameter("title");
+		String content = req.getParameter("content");
+		
+		out.println(title);
+		out.println(content);
 
-		int cnt = 100;
-		if(tmp != null && !tmp.equals("")) {
-			cnt = Integer.parseInt(tmp);
-		}
-
-		for(int i = 0; i < cnt; i++) {			
-			out.println((i+1) + " : 안녕 Servlet!! <br />");
-		}
 	}
 }
