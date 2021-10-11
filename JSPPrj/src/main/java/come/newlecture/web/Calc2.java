b5055276341c9477c1cd09aa5a131cf2aaaa9607
Lily <helloimlily@gmail.com>
2021-10-11 14:06:19 +0900

    1011 Cookie

--- JSPPrj/src/main/java/come/newlecture/web/Calc2.java
+++ JSPPrj/src/main/java/come/newlecture/web/Calc2.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,6 +20,9 @@ public class Calc2 extends HttpServlet {
 		
 		// session을 이용
 		HttpSession session = req.getSession();
+		
+		// cookie를 이용
+		Cookie[] cookies = req.getCookies();
 		
 		resp.setCharacterEncoding("UTF-8"); 
 		resp.setContentType("text/html; charset=UTF-8"); 
@@ -35,10 +39,27 @@ public class Calc2 extends HttpServlet {
 		// 계산
 		if(op.equals("=")) {
 //			int x = (Integer)application.getAttribute("value");
-			int x = (Integer)session.getAttribute("value");
+//			int x = (Integer)session.getAttribute("value");
+			
+			int x = 0;
+			for(Cookie c : cookies) {
+				if(c.getName().equals("value")) {
+					x = Integer.parseInt(c.getValue());
+					break;
+				}
+			}
+			
 			int y = v;
 //			String operator = (String)application.getAttribute("op");
-			String operator = (String)session.getAttribute("op");
+//			String operator = (String)session.getAttribute("op");
+			String operator = "";
+			
+			for(Cookie c : cookies) {
+				if(c.getName().equals("op")) {
+					operator = c.getValue();
+					break;
+				}
+			}
 			
 			int result = 0; 
 
@@ -57,8 +78,15 @@ public class Calc2 extends HttpServlet {
 //			application.setAttribute("op", op);
 			// Map에 필요한 값들을 저장
 			
-			session.setAttribute("value", v);
-			session.setAttribute("op", op);
+//			session.setAttribute("value", v);
+//			session.setAttribute("op", op);
+			
+			Cookie valueCookie = new Cookie("value", String.valueOf(v));
+			Cookie opCookie = new Cookie("op", op);
+			
+			// response header에 심어져서 전달
+			resp.addCookie(valueCookie);
+			resp.addCookie(opCookie);
 			
 		}
 
