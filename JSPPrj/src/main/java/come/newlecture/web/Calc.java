87a005a33eef7f7658d4caf7c87df1b31d44af89
Lily <helloimlily@gmail.com>
2021-10-11 10:57:08 +0900

    1011 입력데이터 배열로 받기

---
+++ JSPPrj/src/main/java/come/newlecture/web/Calc.java
@@ -0,0 +1,46 @@
+package come.newlecture.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/calc")  
+public class Calc extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		resp.setCharacterEncoding("UTF-8"); 
+		resp.setContentType("text/html; charset=UTF-8"); 
+
+		String xStr = req.getParameter("x");
+		String yStr = req.getParameter("y");
+		String op = req.getParameter("operator");
+				
+		int x = 0;
+		int y = 0;
+		
+		if(!xStr.equals("")) {
+			x = Integer.parseInt(xStr);
+		}
+		
+		if(!yStr.equals("")) {
+			y = Integer.parseInt(yStr);
+		}
+		
+		int result = 0; 
+		
+		if(op.equals("더하기")) {
+			result = x + y;
+		} else {
+			result = x - y;
+		}
+		
+		resp.getWriter().printf("result is %d\n", result);
+		
+	}
+}
